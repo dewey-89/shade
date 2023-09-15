199afce5af4caffc16ec3679bb972f1b5dd4ffad
dewey-89 <ksw270@kakao.com>
2023-09-15 21:46:34 +0900

    init commit

---
+++ src/main/java/com/sparta/miniproject/SpartaMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpartaMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/miniproject/SpartaMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
